9ed3f92c244c931ea1818ce51c2c1273253f0ac7
Rodrigo-Casella <r.casella2@studenti.unipi.it>
2023-03-11 13:10:34 +0100

    live logging create multiples files. filter option

--- README.md
+++ README.md
@@ -6,13 +6,14 @@ Script per catturare pacchetti TCP SYN e TLS Client Hello da scrivere in un file
 
 - Python 3.8+
 - <code>apt-get install libpcap-dev</code>
+- <code>python3 -m pip install 'Cython>=0.29.33'</code>
 - <code>python3 -m pip install -r requirements.txt</code>
 
 ## Usage
 
-<code>sudo -E python3 capture_fp.py -i interface|pcap -d path/to/logs/dir [-l log_interval] [-t time]</code>
+<code>sudo -E python3 capture_fp.py -i interface|pcap -d path/to/logs/dir [-l log_interval] [-t time] [-f filter] [-v verbose]</code>
 
-E' possibile configurare l'intervallo di log ed il tempo totale di cattura per le catture live.
+E' possibile configurare l'intervallo di logging ed il tempo totale di cattura per le catture live ed il filtro da applicare.
 
 Example:
 
@@ -22,15 +23,16 @@ CTRL + C per arrestare lo script
 
 <code>sudo -E python3 capture_fp.py -i eth0 -d logs -l 60 -t 300</code>
 
-Viene prodotto un log ogni 60 secondi per un totale di 300 secondi
+Viene prodotto un file log ogni 60 secondi per un totale di 300 secondi
 
 <code>sudo -E python3 capture_fp.py -i capture.pcap -d logs</code>
 
 ## Output
 
-Lo script produce un file di log in cui vengono riporati l'indirizzo IP sorgente assieme alla tupla \<ttl,window size,tcp options> o la lista dei cifrari presenti nel Client Hello
+Lo script produce un file di log ogni <b>log_interval</b> secondi (60 secondi di default) in cui vengono riporati l'indirizzo IP sorgente assieme alla tupla \<ttl,window size,tcp options> o la lista dei cifrari presenti nel Client Hello
 
 ```log
+[08/Mar/2023:12:41:07 +0100]
 172.27.5.66: 64,64240,[(2, 1460), (4, 0), (8, 17850703239813529600), (1, 0), (3, 7)]
 172.27.5.66: [4866, 4867, 4865, 4868, 49196, 52393, 49325, 49162, 49195, 49324, 49161, 49200, 52392, 49172, 49199, 49171, 157, 49309, 53, 156, 49308, 47, 159, 52394, 49311, 57, 158, 49310, 51]
 172.27.5.66: 64,64240,[(2, 1460), (4, 0), (8, 13103132311635886080), (1, 0), (3, 7)]
--- capture_fp.py
+++ capture_fp.py
@@ -1,27 +1,29 @@
 import argparse
+import os
 import socket
 import time
-from os import sep
 
 import dpkt
-from pylibpcap.base import Sniff
 from pylibpcap import OpenPcap
+from pylibpcap.base import Sniff
 from pylibpcap.exception import LibpcapError
 
 TLS_HANDSHAKE = 22
 TLS_CLIENT_HELLO = 1
 
-# to capture only TCP SYN packets or Client Hello records in TLS Handshakes with VLAN tag
-BPF_FILTER = "vlan and (tcp[tcpflags] = tcp-syn or (tcp[tcp[12]/16*4]=22 and (tcp[tcp[12]/16*4+5]=1)))"
 
-def log(log_file, packets):
+def print_verbose(to_print, msg):
+    if to_print:
+        print(msg)
+        
+def log_pkts(log_file, packets):
     if packets:
         for packet in packets:
-            log_file.write(f"{packet[0]}: {packet[1]}\n")
+            log_file.write(f'{packet}\n')
         log_file.flush()
 
 
-def read_pkt(packets: list, plen, buf):
+def read_pkt(packets: list, plen, buf, verbose):
     if plen <= 0:
         return
     try:
@@ -42,51 +44,54 @@ def read_pkt(packets: list, plen, buf):
 
     if tcp.flags == dpkt.tcp.TH_SYN:
         tcp_opts = []
-        
+
         for opt in dpkt.tcp.parse_opts(tcp.opts):
             tcp_opts.append((opt[0], int.from_bytes(opt[1], 'big')))
-            
+
         features = f"{ip.ttl},{tcp.win},{tcp_opts}"
     elif len(tcp.data) > 0 and tcp.data[0] == TLS_HANDSHAKE:
         try:
             record = dpkt.ssl.TLSRecord(tcp.data)
         except dpkt.dpkt.NeedData:
             return
-        
+
         if len(record.data) < 4:
             return
-        
+
         if record.data[0] != TLS_CLIENT_HELLO or int.from_bytes(record.data[1:4], byteorder='big') < 34:
             return
-        
+
         try:
             handshake = dpkt.ssl.TLSHandshake(record.data)
         except (dpkt.ssl.SSL3Exception, dpkt.dpkt.NeedData, dpkt.dpkt.UnpackError):
             return
-        
+
         client_hello: dpkt.ssl.TLSClientHello = handshake.data
-            
+
         features = f"{[x.code for x in client_hello.ciphersuites]}"
-            
+
     if features is not None:
-        packets.append((src_ip, features))
+        pkt = f"{src_ip}: {features}"
+        print_verbose(verbose, pkt)
+        packets.append(pkt)
 
 
-def offline_capture(input, log_file):
-    capture = OpenPcap(input, mode="r", filters=BPF_FILTER)
+def offline_capture(input: str, logs_dir, bpf_filter, verbose):
+    capture = OpenPcap(input, mode="r", filters=bpf_filter)
 
     packets = []
-    try:
-        for plen, ts, buf in capture.read():
-            read_pkt(packets, plen, buf)
-        log(log_file, packets)
-    except KeyboardInterrupt:
-        pass
+    with open(f'{logs_dir}{os.sep}{os.path.basename(input.rstrip(".pcap"))}.log', mode="w") as log_file:
+        try:
+            for plen, ts, buf in capture.read():
+                read_pkt(packets, plen, buf, verbose)
+            log_pkts(log_file, packets)
+        except KeyboardInterrupt:
+            pass
 
 
-def live_capture(input, tot_capture_time, log_interval, log_file):
+def live_capture(input, tot_capture_time, log_interval, logs_dir, bpf_filter, verbose):
     try:
-        sniff = Sniff(input, filters=BPF_FILTER, promisc=1, timeout=5000)
+        sniff = Sniff(input, filters=bpf_filter, promisc=1, timeout=2000)
     except LibpcapError as e:
         print(e)
         exit(1)
@@ -95,50 +100,64 @@ def live_capture(input, tot_capture_time, log_interval, log_file):
     end_time = start_time + tot_capture_time
 
     packets = []
-
-    while time.time() < end_time or not tot_capture_time:
-        capture_start_time = time.time()
-        capture_end_time = capture_start_time + log_interval
-
-        try:
-            for plen, ts, buf in sniff.capture():
-                read_pkt(packets, plen, buf)
-
-                if ts > capture_end_time:
-                    break
-            log(log_file, packets)
-            packets = []
-        except KeyboardInterrupt:
-            break
-
-    log(log_file, packets)
+    i = 1
+    while not tot_capture_time or time.time() < end_time:
+        with open(f'{logs_dir}{os.sep}{input}_{i}.log', mode="w") as log_file:
+            log_file.write(f'[{time.strftime("%d/%b/%Y:%H:%M:%S %z")}]\n')
+            capture_start_time = time.time()
+            capture_end_time = capture_start_time + log_interval
+            try:
+                for plen, ts, buf in sniff.capture():
+                    read_pkt(packets, plen, buf, verbose)
+                    if ts > capture_end_time:
+                        break
+                log_pkts(log_file, packets)
+                packets = []
+            except KeyboardInterrupt:
+                log_pkts(log_file, packets)
+                break
+        i += 1
 
 
 def main():
     parser = argparse.ArgumentParser()
+
     parser.add_argument("-i", "--input", required=True,
                         help="network interface to capture packets from or pcap file to read")
     parser.add_argument("-d", "--dir", required=True,
                         help="path to log dir")
     parser.add_argument("-l", "--log_interval", type=int, default=60,
-                        help="capture time before to write the log file (in seconds)")
+                        help="capture time before to flush the log file (in seconds)")
     parser.add_argument("-t", "--time", type=int, default=0,
                         help="total time to capture packets (in seconds)")
+    parser.add_argument("-f", "--filter", type=str,
+                        help="bpf filter to apply (default will capture all tcp-syn and tls client hello packet with vlan tag)")
+    parser.add_argument("-v", "--verbose",
+                        help="enable verbose mode", action="store_true")
+
     args = parser.parse_args()
 
     input: str = args.input
 
     log_interval = args.log_interval
 
-    log_filename = f"{args.dir}{sep}capture.log"
-    log_file = open(log_filename, "w")
+    logs_dir = args.dir
+    
+    verbose = args.verbose
+
+    # to capture only TCP SYN packets or Client Hello records in TLS Handshakes with VLAN tag
+    bpf_filter = "vlan and (tcp[tcpflags] = tcp-syn or (tcp[tcp[12]/16*4]=22 and (tcp[tcp[12]/16*4+5]=1)))"
+
+    if args.filter is not None:
+        bpf_filter = args.filter
+
+    if not os.path.isdir(logs_dir):
+        os.makedirs(os.path.relpath(logs_dir), mode=0o755, exist_ok=True)
 
     if input.endswith(".pcap"):
-        offline_capture(input, log_file)
+        offline_capture(input, logs_dir, bpf_filter, verbose)
     else:
-        live_capture(input, args.time, log_interval, log_file)
-
-    log_file.close()
+        live_capture(input, args.time, log_interval, logs_dir, bpf_filter, verbose)
 
 
 if __name__ == '__main__':
